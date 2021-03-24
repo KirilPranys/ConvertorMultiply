fca11ee7bd3badfb2b85471baf6dc82d7f801a1e
KirilPranys <steamkiril@gmail.com>
2021-03-24 19:39:50 +0100

    Initial Commit

---
+++ ConvertorMultiple/ConvertorMultipleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertorMultipleApp.swift
+//  ConvertorMultiple
+//
+//  Created by Kiril Pranys on 24.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ConvertorMultipleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
